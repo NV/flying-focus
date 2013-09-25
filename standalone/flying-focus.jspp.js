6493a33ac8c32e43e1593de9b3f7f90069a0d492
Nikita Vasilyev <me@elv1s.ru>
2013-09-25 05:49:15 -0400

    Initialize on DOMContentLoaded
    Make it work when the script included in the <head>.
    Including <script src="flying-focus.js" async></script> at the bottom of the <body>
    is still recomended.

--- standalone/flying-focus.jspp.js
+++ standalone/flying-focus.jspp.js
@@ -1,11 +1,9 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function(e) {
+	if (document.getElementById('flying-focus')) return;
 
-if (document.getElementById('flying-focus')) return;
+	/*> ../chrome/flying-focus.js */
 
-/*> ../chrome/flying-focus.js */
-
-var style = document.createElement('style');
-style.textContent = "/*> ../chrome/flying-focus.css */";
-document.body.appendChild(style);
-
-})();
+	var style = document.createElement('style');
+	style.textContent = "/*> ../chrome/flying-focus.css */";
+	document.body.appendChild(style);
+}, false);
